df7380b0acc67f5b486742672ad63705ee406471
GabrielYacubovich <51945316+GabrielYacubovich@users.noreply.github.com>
2023-04-29 18:20:14 +0300

    Add files via upload

---
+++ bullets.js
@@ -0,0 +1,43 @@
+﻿import { canvas } from './game.js'; 
+
+class Bullet {
+  constructor(x, y, rotation, doubleShooting, isPlayerBullet = true, type = "", bulletSpeed = 10) {
+    this.x = x;
+    this.y = y;
+    this.rotation = rotation;
+    this.size = 5;
+    this.speed = 0;
+    this.speed = bulletSpeed;
+    this.doubleShooting = doubleShooting;
+    this.isPlayerBullet = isPlayerBullet;
+    this.type = type;
+  }
+
+  update() {
+    this.x += this.speed * Math.sin(this.rotation);
+    this.y -= this.speed * Math.cos(this.rotation);
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.rotation);
+    ctx.beginPath();
+    ctx.arc(0, 0, this.size / 2, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fillStyle = this.isPlayerBullet ? "white" : "red";
+    ctx.fill();
+    ctx.restore();
+  }
+
+  offscreen() {
+    return (
+      this.x < 0 ||
+      this.x > canvas.width ||
+      this.y < 0 ||
+      this.y > canvas.height
+    );
+  }
+}
+
+export { Bullet };
---
+++ powerup.js
@@ -0,0 +1,73 @@
+﻿  import { incrementScore } from './game.js';
+
+  // powerup.js
+  class Powerup {
+    constructor(x, y, type) {
+      this.x = x;
+      this.y = y;
+      this.type = type;
+      this.width = 20;
+      this.height = 20;
+      this.markForDeletion = false;
+    }
+    draw(ctx) {
+      ctx.save();
+      ctx.beginPath();
+      ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+      ctx.fillStyle = this.type === 'invulnerability' ? 'green' : this.type === 'extraLife' ? 'blue' : this.type === 'doubleShooting' ? 'orange' : this.type === 'slowMotion' ? 'purple' : 'pink';
+      ctx.fill();
+      ctx.restore();
+    }
+    update(player, lives, asteroids, enemies) {
+      let distance = Math.sqrt((this.x - player.x) ** 2 + (this.y - player.y) ** 2);
+      if (distance < (this.width / 2 + player.width / 2)) {
+        this.applyEffect(player, lives, asteroids, enemies);
+        this.markForDeletion = true;
+      }
+    }
+    
+    applyEffect(player, lives, asteroids, enemies) {
+      if (this.type === 'invulnerability') {
+        player.invulnerable = true;
+        player.invulnerabilityTime = Date.now();
+        player.color = 'green';
+      
+      } else if (this.type === 'extraLife') {
+        player.lives++;
+        player.livesLeft++;      
+
+      } else if (this.type === 'doubleShooting') {
+        player.doubleShooting = true;
+        player.doubleShootingTime = Date.now();
+        player.color = 'orange';
+        player.bulletSpeed = player.originalBulletSpeed * 2;
+        player.doubleShootingLost = false;
+          
+      } else if (this.type === 'slowMotion') {
+        if (asteroids) {
+          for (const asteroid of asteroids) {
+            asteroid.slowMotion = true;
+          }
+
+        }
+      }
+        // Comment out or delete the following block to remove the enemies from the slow-motion effect
+        // if (enemies && Array.isArray(enemies)) {
+        //   for (const enemy of enemies) {
+        //     enemy.slowMotion = true; // Set the slowMotion property to true
+        //   }
+        // }
+    
+      else if (this.type === 'backfront') {
+        player.backfrontShooting = true;
+        player.backfrontShootingTime = Date.now();
+        player.color = 'pink';
+      }
+      incrementScore(30);
+    }
+    
+
+  }
+
+  export { Powerup };
+
